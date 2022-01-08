d96725802961f3ded1a6ba810ecf303ecf76b9d8
Jakub Chłapiński <jakub.chlapinski@kodegenix.pl>
2022-01-08 17:51:37 +0100

    fixed resolving symlinks

--- bin/esmfix.js
+++ bin/esmfix.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import path from 'path'
+import fs from "fs";
 import { spawn } from 'child_process'
 
 const node = process.argv[0]
-const scriptFile = path.resolve(path.dirname(process.argv[1]), '../lib/cli.js')
+const binFile = fs.realpathSync(process.argv[1])
+const scriptFile = path.resolve(path.dirname(binFile), '../lib/cli.js')
 const args = [...process.argv.slice(2)]
 
 const p = spawn(node, ['--experimental-import-meta-resolve', scriptFile, ...args], {
